4ada5c6dd571472dd9e8e08563218c9617d60d3c
Robert Einhaus <robert@einhaus.info>
2017-05-06 11:22:56 +0200

    fixed install of zim

--- install.sh
+++ install.sh
@@ -138,12 +138,7 @@ if [ "$ZSH_INSTALLED" = true ]; then
     if [ `command -v git` ]; then
         echo "-> installing ZIM"
         git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
-#        setopt EXTENDED_GLOB
-#        for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/* ); do
-#            user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
-#            touch ${user_file}
-#            ( print -rn "$(<${template_file})$(<${user_file})" >! ${user_file} ) 2>/dev/null
-#        done
+        zsh ${DIR}/install_zim.sh
     fi
     ZSHRC="${DIR}/.zshrc"
     if [ -f ${HOME}/.zshrc ]; then
--- install_zim.sh
+++ install_zim.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 setopt EXTENDED_GLOB
-for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/* ); do
+for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/{zimrc,zlogin} ); do
     user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
     touch ${user_file}
     ( print -rn "$(<${template_file})$(<${user_file})" >! ${user_file} ) 2>/dev/null
