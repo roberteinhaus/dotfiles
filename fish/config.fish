09d3ce8de8c139d9cf5648175979e65a2cef7b1a
Robert Einhaus <robert@einhaus.info>
2017-10-19 11:28:12 +0200

    Fixed tmux startup an colors for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,13 @@
 set fish_greeting ''
 fish_vi_key_bindings
 set theme_color_scheme gruvbox
+command -qs tmux
+if test $status -eq 0; and test -z "$TMUX"
+    if test "$SSH_CONNECTION" != ""
+        tmux -2 attach-session -t ssh; or tmux -2 new-session -s ssh; and exit
+    else
+        tmux -2 new-session; and exit
+    end
+else
+    source ~/.sh_customvars
+end
